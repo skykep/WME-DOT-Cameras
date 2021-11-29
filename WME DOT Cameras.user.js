b44c73e066bd6ab524c8fe111d3e0dd925de5baf
Matt Keperling <mattkeperling@gmail.com>
2021-11-29 14:58:50 -0500

    Fixed again

--- WME DOT Cameras.user.js
+++ WME DOT Cameras.user.js
@@ -11,7 +11,7 @@
 // @grant        GM_info
 // @grant        GM_fetch
 // @grant        GM_addStyle
-// @require      https://cdn.jsdelivr.net/npm/hls.js@latest
+// @require      https://cdnjs.cloudflare.com/ajax/libs/hls.js/1.1.2-0.canary.8064/hls.js
 // @require      https://unpkg.com/video.js/dist/video.js
 // @require      https://unpkg.com/@videojs/http-streaming/dist/videojs-http-streaming.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/x2js/1.2.0/xml2json.min.js
